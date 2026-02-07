3894de3fe6bfea284b00b2ae15c1a607c422cedc
srilalithakothuri <kothurisrilalitha@gmail.com>
2026-02-07 12:30:53 +0530

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DashboardLayout from './layouts/DashboardLayout';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ChatPage from './pages/ChatPage';
+import LibraryPage from './pages/LibraryPage';
+import TasksPage from './pages/TasksPage';
+import AnalyticsPage from './pages/AnalyticsPage';
+import DocSpacePage from './pages/DocSpacePage';
+import ProfilePage from './pages/ProfilePage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="chat" element={<ChatPage />} />
+          <Route path="library" element={<LibraryPage />} />
+          <Route path="docs" element={<DocSpacePage />} />
+          <Route path="tasks" element={<TasksPage />} />
+          <Route path="analytics" element={<AnalyticsPage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="settings" element={<ProfilePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
